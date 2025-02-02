0043b24fb6bf8eca823296f1aaab85b2e9009579
Olegshkradiuk <Olegwills40@gmail.com>
2025-02-02 20:53:58 +0200

    js

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-let a=2;
-a="abc";
-console.log(a);
-const b =3;
+// let a=2;
+// a="abc";
+// console.log(a);
+// const b =3;
 //  b = "lmn"; -const cannit be updated
+//  primirive types
+/*
+1. number  like double in Java
+2. string "abc" < "b"
+3. boolean (true, false)
+4. undefined (undefined)
+5. null (null) 
+
+*/
+//  1. number
