43c967a322ba523a3c1b8d530e0f2f0751463c13
Olegshkradiuk <Olegwills40@gmail.com>
2025-02-05 20:39:08 +0200

    function with primitives & template string

--- index.js
+++ index.js
@@ -1,36 +1,6 @@
+function sum(num1, num2="1") {
+    return num1 + num2;
+}
 
-// strings: *********
-// let str = "hello";
-// str = str.at.toLowerCase();
-// console.log(str);
-// let b = str < "kuku";
-// console.log(b);
-// **********************************************
-// comparing strings: if both are strings the comparing will be according to codes ASCII 
-// if one is a number the second will be coverted to a number as well
-// if the second string cannot be converted to a number the specific value NaN will give always false except operation 
-// **********************************************
-// let a9 = "20";
-// let str1 = a9;
-// let str2 = "9";
-// let b = str1 < str2;
-// console.log (b);
-
-// ********* 
-// let str1 = "2a";
-// let str2 = "-5";
-// console.log(str1 - str2);
-
-// let str = true;
-// if(NaN) { 
-//     console.log("" + 5 * 2);;
-// }
-
-// // ************************
-// console.log(null - "123"); // -123
-/****************************** */
-// console.log(true + false);
-
-// **********************************
-
-console.log(123 + +"true");
+console.log(sum(1,1) + sum(20, 30)); 
+console.log(`sum(10, 20) = ${sum( 10, 20)}`) /* +sum(10, 20) = 30 */
