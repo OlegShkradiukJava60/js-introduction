5bc88a33a723296191e1c0c250058767f63cf7d9
Olegshkradiuk <Olegwills40@gmail.com>
2025-02-08 20:32:24 +0200

    js_hw

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-function sumDigits(num) {
-    // TODO 
-    //returns sum of the digits in the given number 
-    // examples: sumDigits(123) => 6
-    // sumDigits ("123") => 6
-    // sumDigits ("a123") => NaN
-    // sumDigits ("123a") => 6
+function sumDigits(str) {
+
+    let sum = 0;
+
+    let num = Math.trunc(Math.abs(+str));
+
+    if (isNaN(num)) {
+        return NaN;
+    }
+
+
+    while (num > 0) {
+        let digit = num % 10;
+        sum += digit;
+        num = Math.trunc(num / 10);
+    }
+    return sum;
 }
 
- 
-function displayAnanas(){
-//TODO 
-// display out word "ananas" 
+console.log (`result:  ${sumDigits(" ")}`) // => 6
+console.log (`result:  ${sumDigits("123")}`) // => 6
+console.log (`result:  ${sumDigits("a123")}`) // => NaN
+console.log (`result:  ${sumDigits("123a")}`) // => NaN
+console.log (`result:  ${sumDigits(-123)}`) // => 6
+console.log (`result:  ${sumDigits(123.3333)}`) // => 6
+
+function displayAnanas() {
+console.log(('a' + -("as") + 'as').toLowerCase());
 }
+ displayAnanas();
