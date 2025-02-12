964fc261604a67cd4ef68b3260fb02b87a2d55ce
Olegshkradiuk <Olegwills40@gmail.com>
2025-02-12 21:09:24 +0200

    HW #13 Definition

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-﻿# HW #12 Definition
+﻿# HW #13 Definition
+## write the method "some" (see comments)
+## write the method "every" (see comments)
+## write more test cases(console.log functoinality)
 
--- index.js
+++ index.js
@@ -1,20 +1,32 @@
-
-// function is an object with operator () (function call)
-// conversion function to string will return string containin text of the function
-//conversion function to number will return NaN
-const sum = function(a,b){
-    return a + b;
-}
-const subtract = function(a,b){
-    return a - b;
-}
-const multiply = function(a,b){
-    return a * b;
-}
-// console.log(sum + "")
-function calculate(fun, a, b){
-    return fun(a,b);
+const array = ["abc", 23  ];
+console.log (array.length)
+console.log ( array[0])
+function forEach (array, fun) {
+    for (let i = 0; i <array.length; i++) {
+        fun(array[i], i);
+    }
 }
 
-console.log (calculate(multiply, 10, sum))
-console.log(10(1, 6))
+//forEach takes array and fun with wto parameters: first - element of array, second - index
+function print(elem, index) {
+    console.log(`index: ${index}, element ${elem}`)
+}
+forEach(array, print);
+function some(array, fun){
+    //TODO
+    //returns true if all elements of the array match a condition given in the function (fun)   
+}
+
+function evenNumber(num) {
+    return num % 2 == 0;
+}
+
+console.log(`using "some" function for even numbers array: ${array}, function ${evenNumber}, result: ${some(array, evenNumber) - false}`)
+function every (array,fun){
+
+}
+
+array = [2, 3, 4];
+function elmGreaterIndex(elem, index) {
+    return elem > index
+}
