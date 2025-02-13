202df97eec822fbff67a763b253b7d548bdd7cb5
Olegshkradiuk <Olegwills40@gmail.com>
2025-02-13 20:15:08 +0200

    HW #13 arrays

--- index.js
+++ index.js
@@ -1,32 +1,30 @@
-const array = ["abc", 23  ];
-console.log (array.length)
-console.log ( array[0])
-function forEach (array, fun) {
-    for (let i = 0; i <array.length; i++) {
-        fun(array[i], i);
+
+function some (array, fun) {
+    for (let i = 0; i < array.length; i++) {
+        if (fun(array[i], i)) {
+            return true;
+        }
+        return false;
     }
 }
-
-//forEach takes array and fun with wto parameters: first - element of array, second - index
-function print(elem, index) {
-    console.log(`index: ${index}, element ${elem}`)
-}
-forEach(array, print);
-function some(array, fun){
-    //TODO
-    //returns true if all elements of the array match a condition given in the function (fun)   
+function every (array, fun) {
+    for (let i = 0; i < array.length; i++) {
+        if (fun(array[i], i)){
+            return true;
+        }
+        return false;
+    }
 }
 
 function evenNumber(num) {
     return num % 2 == 0;
 }
 
-console.log(`using "some" function for even numbers array: ${array}, function ${evenNumber}, result: ${some(array, evenNumber) - false}`)
-function every (array,fun){
-
-}
-
-array = [2, 3, 4];
 function elmGreaterIndex(elem, index) {
-    return elem > index
+    return elem > index;
 }
+
+
+let array = [2, 3, 4];
+console.log (`some function for even numbers: ${some(array, evenNumber)}`);
+console.log(`every to check elements greater than index: ${every(array, elmGreaterIndex)}`);
