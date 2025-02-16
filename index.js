312e1afed58efaa4b1c5d504085c97bd20b6faa8
Olegshkradiuk <Olegwills40@gmail.com>
2025-02-16 19:12:10 +0200

    hw done

--- index.js
+++ index.js
@@ -28,18 +28,18 @@ function elmGreaterIndex(elem, index) {
     return elem > index;
 }
 
-let array = [2, 3, 4];
+const array = [2, 3, 4];
 console.log(`some function for even numbers: ${some(array, evenNumber)} - true`);
 console.log(`every to check elements greater than index: ${every(array, elmGreaterIndex)} - true`);
 
-let array2 = [1, 3, 5];
+const array2 = [1, 3, 5];
 console.log(`some() to check even numbers (all odd): ${some(array2, evenNumber)} - false`);
 console.log(`every() to check elements greater than index (all greater): ${every(array2, elmGreaterIndex)} - true`);
 
-let array3 = [0, 2, 4, 6];
+const array3 = [0, 2, 4, 6];
 console.log(`some() to check even numbers (all even): ${some(array3, evenNumber)} - true`);
 console.log(`every() to check even numbers (all even): ${every(array3, evenNumber)} - true`);
 
-let array4 = [0, 1, 2, 3];
+const array4 = [0, 1, 2, 3];
 console.log(`some() to check elements greater than index: ${some(array4, elmGreaterIndex)} - false`);
 console.log(`every() to check elements greater than index: ${every(array4, elmGreaterIndex)} - false`);
