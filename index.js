7eaa1a115e6fe93ad181727a7ce5a4c5e64bea02
Olegshkradiuk <Olegwills40@gmail.com>
2025-02-19 20:01:31 +0200

    c

--- index.js
+++ index.js
@@ -1,32 +1,28 @@
+const array = ['Hello', 122, -10, 'js', 500, 'Nodejs'];
 
-function minMax(...operands) {
-    let min = Infinity;
-    let max = -Infinity;
+//adding to array
+// push adds to end of an source array
+// unshift adds at beginnig of an source array
+// splice inserts at middle of array 
 
-    function numbersMinMax(num) {
-        if (num < min) {
-            min = num;
-        }
-        if (num > max) {
-            max = num;
-        }
-    }
+// array.push(... [1, 2, 3]);
+// console.log (array);
+// console.log([1, 2, 3].push([4, 5, 6]))
+// const array1 = array.concat(3, 4, 5);
+// const array1 = [1,2,3].concat(3,4,5)
+// console.log(array1[3])
+// const ar1 = [3,4,5].concat(array)
+// console.log (ar1)
 
+// console.log(array.unshift(3, 4, 5))
+// console.log(array);
 
-    for (let i = 0; i < operands.length; i++) {
-        let operand = operands[i];
+// console.log(array.splice(3,2))
 
-        if (Array.isArray(operand)) {
-            for (let j = 0; j < operand.length; j++) {
-                numbersMinMax(operand[j]);
-            }
-        } else if (typeof operand === "number" && isFinite(operand)) {
-            numbersMinMax(operand);
-        }
-    }
-    return [min, max];
-}
+//********** ************************ ***** */
 
-console.log(`minMax(1, 2, 3)=${minMax(1, 2, 3)}`);
-console.log(`minMax(1, 2, 3, [100, 50]=${minMax(1, 2, 3, [100, 50])}`);
-console.log(`minMax(1, 2, 3, [100, 50], [-2, 40, 200])=${minMax(1, 2, 3, [100, 50], [-2, 40, 200])}`);
+// removing last element
+// console.log(array.pop())
+// console.log(array)
+// removing first element
+console.log (array.shift())
