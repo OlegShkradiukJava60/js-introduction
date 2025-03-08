59fc0ad55856d7dc0e3201a08e026a993fa5f668
Olegshkradiuk <Olegwills40@gmail.com>
2025-03-08 18:37:57 +0200

    hw19 08.03

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 import { testframework } from "./testframework.js";
-testframework('occurrences function test', 'const strings = ["b","a", "c", "b", "b", "c"]',
-  ["occurrences(strings)"], [["a=>1", "b=>3", "c=>2"]])
-testframework('isAnagram testing', 'const string = "hello"',
-  ['isAnagram(string, "olleh")', 'isAnagram(string, "lehol")',
-    'isAnagram(string, "ollle")', 'isAnagram(string, "hell")',
-    'isAnagram(string, "oelhl")',
-    
-  ], [true, true, false, false, true]
-)
+
+let first = 1;
+let second = 2;
+
+[first, second] = [second, first];
+
+console.log(`second = ${second}`); 
+console.log(`first = ${first}`);  
