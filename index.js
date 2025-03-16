d30d0c5377f534f241b0febf37c5990425b35468
Olegshkradiuk <Olegwills40@gmail.com>
2025-03-16 20:58:36 +0200

    sleep method

--- index.js
+++ index.js
@@ -1,65 +1,16 @@
-import myBind from './myBind.js';
 
-class Deferred {
-  constructor() {
-    this.chain = [];
-    this.value = undefined;
-  }
-
-  // добавялем фунцкию // add function
-  then(callback) {
-    this.chain.push(callback);
-    return this;
-  }
-
-  // start chain
-  resolve(value) {
-    this.value = value; // first value
-
-    this.chain.forEach((callback) => {
-      this.runCallback.bind(this)(callback);
-    });
-  }
-
-  runCallback(callback) {
-    this.value = callback(this.value);  // induce callback and upgrate value
-  }
+function sleep(timeout) {
+  const rejectData = 5
+  return new Promise(resolve => {
+    if (timeout < 0) {
+      throw ("timeout cannot be negative");
+    }
+    setTimeout(() => resolve(), timeout)
+  })
 }
-
-// examples
-
-const o = new Deferred();
-
-o.then(function (res) {
-  console.log("1", res); // hello world
-  return "a"
-})
-o.then(function (res) {
-  console.log("2", res); // a
-  return "b"
-})
-o.then(function (res) {
-  console.log("3", res); // b
-  return "c"
-})
-
-
-o.resolve("hello world"); // start chain
-
-
-
-//  MyBind
-
-
-function personInfo(age, country) {
-  console.log(
-    `Hello, my name is ${this.name}. I'm ${age} years old and live in ${country}.`
-  );
-}
-
-const person = {
-  name: "Oleg"
-};
-const personInfoOleg = personInfo.myBind(person, 28);
-
-personInfoOleg("Israel");
+const timeout = 5000;
+sleep(timeout).then(() => console.log(`result after ${timeout / 1000} seconds`))
+  .catch(e => { console.log(e); return 5 })
+  // .then(data => console.log(data))
+  .finally(() => console.log("printing out in any case"))
+console.log("Hello World")
