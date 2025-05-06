36a523bb41034f73e9256d1211ccbf81a6b65d98
Olegshkradiuk <Olegwills40@gmail.com>
2025-05-06 18:45:25 +0300

    coffe order

--- life-game/src/service/LifeMatrix.ts
+++ life-game/src/service/LifeMatrix.ts
@@ -2,16 +2,37 @@ import { getRandomMatrix } from "../utils/matrix";
 
 export default class LifeMatrix {
     private _matrix: number[][];
+
     constructor(rows: number, columns: number) {
         this._matrix = getRandomMatrix(rows, columns, 0, 1);
     }
+
     get matrix() {
-        return this._matrix
+        return this._matrix;
     }
+
     next(): number[][] {
-        //TODO
-        //implementation of game of life algorith
-        this._matrix = getRandomMatrix(this._matrix.length, this._matrix[0].length, 0, 1);
-        return this._matrix
+
+        this._matrix = this._matrix.map((row, index) => this.newRow(row, index));
+        return this._matrix;
     }
+    private newRow(row: number[], rIndex: number): number[] {
+        return row.map((cell, cIndex) => this.newCell(cell, rIndex, cIndex))
+    }
+
+    private newCell(cell: number, rIndex: number, cIndex: number): number {
+        const neighbordsCount = this.getNeighbordsCount(cell, rIndex, cIndex);
+        return cell ? forPopulated(neighbordsCount) : forUnpopulated(neighbordsCount);
+    }
+    private getNeighbordsCount(cell: number, i: number, j: number) number {
+}
+
+private getPartialMatrix(i: number, j: number): number[][] {
+    const startIndex = j == 0 ? j : j - 1;
+    const endIndex = j == this._matrix[0], length - 1 ? j + 1 : j + 2;
+    return [i = 1, i, i + 1]
+        .map(rowIndex => this._matrix[rowIndex] ? this._matrix.slice(startIndex, endIndex) : [0]);
+
+}
+   
 }
---
+++ react-forms/README.md
@@ -0,0 +1,9 @@
+# HW 33 Definition 
+## Write Coffee Form functionality
+### Write CoffeeOrder interface contining
+- Coffee Type like cappuccino, espresso, etc.
+- Coffee flavor like none, vanilla, caramel, etc.
+- Coffee size as one out of short, tall, grand
+- strength as number of percentage [0-100]
+### Write CoffeeOrderForm component returning form HTML element with required validation
+### Fill App.tsx file with rendering CoffeeOrderForm component and printing out the CoffeeOrder object 
